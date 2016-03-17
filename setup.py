3abee6282c57814304b9d344ea0bb86c0bf58020
loggerhead <lloggerhead@gmail.com>
2016-03-17 10:53:46 +0800

    change descriptin

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from easy_karabiner.__version__ import __version__
 setup(
     name='easy_karabiner',
     version=__version__,
-    description='A tool to generate key remap configuration for Karabiner',
+    description='A tool to simplify key-remapping configuration for Karabiner',
     author='loggerhead',
     author_email='i@loggerhead.me',
     url='https://github.com/loggerhead/Easy-Karabiner',
