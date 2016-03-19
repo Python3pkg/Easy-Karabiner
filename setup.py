5979a33a02b36e1482c7db0be2adf5dfdb859a84
loggerhead <lloggerhead@gmail.com>
2016-03-19 17:41:03 +0800

    add more explicit classifiers

--- setup.py
+++ setup.py
@@ -28,7 +28,11 @@ setup(
         'Operating System :: MacOS',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Utilities',
     ]
 )
