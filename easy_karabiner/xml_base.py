bac609de96af159d71d6d130870ecb47911d1d7a
loggerhead <lloggerhead@gmail.com>
2016-03-21 22:06:16 +0800

    change XML_base methods order

--- easy_karabiner/xml_base.py
+++ easy_karabiner/xml_base.py
@@ -6,6 +6,17 @@ from easy_karabiner import exception
 
 class XML_base(object):
     @staticmethod
+    def parse(filepath):
+        return etree.parse(filepath).getroot()
+
+    @staticmethod
+    def parse_string(xml_str):
+        return etree.fromstring(xml_str)
+
+    def get_clsname(self):
+        return self.__class__.__name__
+
+    @staticmethod
     def create_cdata_text(text):
         return etree.CDATA(text)
 
@@ -14,23 +25,6 @@ class XML_base(object):
         et = etree.Element(name, **kwargs)
         et.text = text
         return et
-
-    @staticmethod
-    def to_format_str(xml_tree, pretty_text=True):
-        indent = "  "
-        if pretty_text:
-            XML_base.pretty_text(xml_tree, indent=indent)
-        xml_string = etree.tostring(xml_tree)
-        xml_string = minidom.parseString(xml_string).toprettyxml(indent=indent)
-        return xml_string
-
-    @staticmethod
-    def parse(filepath):
-        return etree.parse(filepath).getroot()
-
-    @staticmethod
-    def parse_string(xml_str):
-        return etree.fromstring(xml_str)
 
     @staticmethod
     def pretty_text(elem, indent="  ", level=0):
@@ -51,8 +45,14 @@ class XML_base(object):
 
         return elem
 
-    def get_clsname(self):
-        return self.__class__.__name__
+    @staticmethod
+    def to_format_str(xml_tree, pretty_text=True):
+        indent = "  "
+        if pretty_text:
+            XML_base.pretty_text(xml_tree, indent=indent)
+        xml_string = etree.tostring(xml_tree)
+        xml_string = minidom.parseString(xml_string).toprettyxml(indent=indent)
+        return xml_string
 
     def to_xml(self):
         raise exception.NeedOverrideError()
